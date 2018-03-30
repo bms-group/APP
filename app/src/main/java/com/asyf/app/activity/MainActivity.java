74b70238f361a265b0e7082fb03cbf1ba037cebc
373616511 <373616511@qq.com>
2018-03-30 17:27:16 +0800

    xutils3

--- app/src/main/java/com/asyf/app/activity/MainActivity.java
+++ app/src/main/java/com/asyf/app/activity/MainActivity.java
@@ -2,23 +2,30 @@ package com.asyf.app.activity;
 
 import android.graphics.Color;
 import android.icu.util.Calendar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.asyf.app.R;
+import com.asyf.app.common.Logger;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarUtils;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.OnRangeSelectedListener;
 import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnDateSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
         EventDecorator eventDecorator = new EventDecorator(Color.parseColor("red"), set);
         calendarView.addDecorator(eventDecorator);
 
+        calendarView.setOnRangeSelectedListener(new OnRangeSelectedListener() {
+            @Override
+            public void onRangeSelected(@NonNull MaterialCalendarView widget, @NonNull List<CalendarDay> dates) {
+                Toast.makeText(MainActivity.this, "点击", Toast.LENGTH_SHORT);
+            }
+        });
+        //时间改变
+        calendarView.setOnDateChangedListener(this);
         /*calendarView.shouldAnimateOnEnter(true)
                 .setFirstDayOfWeek(Calendar.MONDAY)
                 .setOnDateClickListener(this::onDateClick)
@@ -43,6 +58,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         calendarView.update(Calendar.getInstance(Locale.getDefault()));*/
+    }
+
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+        Logger.e("aa","saasahduiqyd78t7e2t测试");
+        Toast.makeText(MainActivity.this, "点击", Toast.LENGTH_SHORT);
+
     }
 
     public class EventDecorator implements DayViewDecorator {
