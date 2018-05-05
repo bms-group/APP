0ad486db0c4dd41811bb954d2590347fee0caa56
373616511 <373616511@qq.com>
2018-05-05 21:47:13 +0800

    代码提交

--- app/src/main/java/com/asyf/app/fragment/ScheduleFragment.java
+++ app/src/main/java/com/asyf/app/fragment/ScheduleFragment.java
@@ -1,14 +1,23 @@
 package com.asyf.app.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.asyf.app.R;
+import com.asyf.app.myView.MyDefinedCalendar;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ScheduleFragment extends Fragment {
 
@@ -25,4 +34,35 @@ public class ScheduleFragment extends Fragment {
         return inflater.inflate(R.layout.fg_schedule, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FragmentActivity activity = getActivity();
+        final MyDefinedCalendar myDefinedCalendar = activity.findViewById(R.id.my_calendar);
+        myDefinedCalendar.setOnDateChangeListener(new MyDefinedCalendar.OnDateChangeListener() {
+            @Override
+            public void onDayClick(String date) {
+                Toast.makeText(getContext(), "您点击了" + date, Toast.LENGTH_SHORT).show();
+                List<String> list = new ArrayList<>();
+                list.add("qwqjhoq" + date);
+                list.add("adssd" + date);
+                list.add("第三行数据" + date);
+                list.add("第4行数据" + date);
+                list.add("第5行数据" + date);
+                list.add("第6行数据" + date);
+                list.add("第7行数据" + date);
+                list.add("第8行数据" + date);
+                myDefinedCalendar.addListData(list);
+                Set<Integer> set = new HashSet<>();
+                set.add(Integer.valueOf(date.split("-")[2]));
+                myDefinedCalendar.addEvents(set);
+            }
+
+            @Override
+            public void onMonthClick(String date) {
+                Toast.makeText(getContext(), "您点击了" + date, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/asyf/app/myView/MyDefinedCalendar.java
+++ app/src/main/java/com/asyf/app/myView/MyDefinedCalendar.java
@@ -12,25 +12,35 @@ import android.widget.Toast;
 
 import com.asyf.app.R;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class MyDefinedCalendar extends LinearLayout {
 
+    private List<String> list;
+    private ArrayAdapter<String> adapter;
+    private MyView myView;
+
     public MyDefinedCalendar(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         View inflate = LayoutInflater.from(context).inflate(R.layout.my_defined_calendar, this);
+        list = new ArrayList<>();
         //要显示的数据
-        String[] strs = {"基神", "B神", "翔神", "曹神", "J神"};
+        // List<String> list = {"基神", "B神", "翔神", "曹神", "J神"};
         //创建ArrayAdapter
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_expandable_list_item_1, strs);
+        adapter = new ArrayAdapter<String>(context, android.R.layout.simple_expandable_list_item_1, list);
         //获取ListView对象，通过调用setAdapter方法为ListView设置Adapter设置适配器
-        ListView list_test = (ListView) findViewById(R.id.my_defined_listView);
-        list_test.setAdapter(adapter);
-        MyView myView = findViewById(R.id.my_defined_my_view);
+        ListView listView = (ListView) findViewById(R.id.my_defined_listView);
+        listView.setAdapter(adapter);
+        myView = findViewById(R.id.my_defined_my_view);
         myView.setOnDateClickListener(new MyView.OnDateClickListener() {
             @Override
             public void onMonthClick(String date) {
+                //日期变化的时候清空列表数据
+                adapter.clear();
+                adapter.notifyDataSetChanged();
                 //Toast.makeText(getContext(), "您点击了" + date, Toast.LENGTH_SHORT).show();
                 if (onDateChangeListener != null)
                     onDateChangeListener.onMonthClick(date);
@@ -38,15 +48,29 @@ public class MyDefinedCalendar extends LinearLayout {
 
             @Override
             public void onDayClick(String date) {
+                adapter.clear();
+                adapter.notifyDataSetChanged();
                 //Toast.makeText(getContext(), "您点击了" + date, Toast.LENGTH_SHORT).show();
                 if (onDateChangeListener != null)
                     onDateChangeListener.onDayClick(date);
             }
         });
-        Set<Integer> set = new HashSet<>();
+        /*Set<Integer> set = new HashSet<>();
         for (int i = 1; i < 32; i++) {
             set.add(i);
         }
+        myView.AddEvents(set);*/
+    }
+
+    public void addListData(List<String> list) {
+        adapter.clear();
+        for (String s : list) {
+            adapter.add(s);
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    public void addEvents(Set<Integer> set) {
         myView.AddEvents(set);
     }
 
@@ -60,7 +84,7 @@ public class MyDefinedCalendar extends LinearLayout {
 
     }
 
-    public void setOnDateClickListener(OnDateChangeListener onDateChangeListener) {
+    public void setOnDateChangeListener(OnDateChangeListener onDateChangeListener) {
         this.onDateChangeListener = onDateChangeListener;
     }
 
