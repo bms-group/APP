758f2c1458912436fe2f4ea2bc1f76920868205d
373616511@qq.com <loveli123>
2018-05-10 17:16:18 +0800

    recycleview

--- app/src/main/java/com/asyf/app/activity/VideoPlayerActivity.java
+++ app/src/main/java/com/asyf/app/activity/VideoPlayerActivity.java
@@ -11,17 +11,17 @@ import cn.jzvd.JZVideoPlayer;
 import cn.jzvd.JZVideoPlayerStandard;
 
 public class VideoPlayerActivity extends AppCompatActivity {
-    String url;
+    public static final String url = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
         JZVideoPlayerStandard jzVideoPlayerStandard = findViewById(R.id.videoplayer);
-        url = "http://192.168.0.130:8080/userfiles/0a99aaaa14f04258b2d9224169e79729/files/book/2018/04/EB0D01505EFEDC6797C416686A8CA7C0.flv";
+        //url = "http://192.168.0.130:8080/userfiles/0a99aaaa14f04258b2d9224169e79729/files/book/2018/04/EB0D01505EFEDC6797C416686A8CA7C0.flv";
         jzVideoPlayerStandard.setUp(url, JZVideoPlayerStandard.SCREEN_LAYOUT_NORMAL, "test");
-        String imageUrl = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1523507435903&di=6976d9937a2e31b6278a4723feb53db5&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2F9f510fb30f2442a7cd0600dbdd43ad4bd1130247.jpg";
-        jzVideoPlayerStandard.thumbImageView.setImageURI(Uri.parse(imageUrl));
+        //String imageUrl = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1523507435903&di=6976d9937a2e31b6278a4723feb53db5&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2F9f510fb30f2442a7cd0600dbdd43ad4bd1130247.jpg";
+        //jzVideoPlayerStandard.thumbImageView.setImageURI(Uri.parse(imageUrl));
         //播放视频
         jzVideoPlayerStandard.startVideo();
     }
--- app/src/main/java/com/asyf/app/fragment/PlanFragment.java
+++ app/src/main/java/com/asyf/app/fragment/PlanFragment.java
@@ -1,14 +1,25 @@
 package com.asyf.app.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.asyf.app.R;
+import com.asyf.app.activity.MessageListActivity;
+import com.asyf.app.entity.MyMessage;
+import com.asyf.app.myView.ChatView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.jiguang.imui.commons.models.IMessage;
+import cn.jiguang.imui.messages.MessageList;
+import cn.jiguang.imui.messages.MsgListAdapter;
 
 
 public class PlanFragment extends Fragment {
@@ -24,11 +35,23 @@ public class PlanFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        FragmentActivity a = getActivity();
+        /*List<String> contacts = new ArrayList<>();
+        contacts.add("鲁班七号");
+        contacts.add("马可波罗");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_expandable_list_item_1, contacts);
+        ListView listView = a.findViewById(R.id.contact);
+        listView.setAdapter(arrayAdapter);*/
+        /*MessageList messageList = a.findViewById(R.id.msg_list);
+        MsgListAdapter.HoldersConfig holdersConfig = new MsgListAdapter.HoldersConfig();
+        MsgListAdapter adapter = new MsgListAdapter<>("0", holdersConfig, null);
+        MyMessage myMessage = new MyMessage("qqq", IMessage.MessageType.SEND_TEXT);
+        List<MyMessage> ms = new ArrayList<>();
+        ms.add(myMessage);
+        adapter.addToEnd(ms);
+        messageList.setAdapter(adapter);*/
+        Intent intent = new Intent(getActivity(), MessageListActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public void onHiddenChanged(boolean hidden) {
-        super.onHiddenChanged(hidden);
-        Toast.makeText(getActivity(), "隐藏", Toast.LENGTH_SHORT).show();
-    }
 }
